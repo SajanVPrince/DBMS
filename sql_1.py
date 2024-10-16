a6c71a0423182103cba429dd3e47bbac83d40162
SajanVPrince <sajanvprince@gmail.com>
2024-10-16 09:27:37 +0530

    mysql

--- sql_1.py
+++ sql_1.py
@@ -23,8 +23,27 @@ except:
     pass
 
 # cur.execute("insert into std (roll_no,name,age)values(1,'akhil',22),(2,'appu',22),(3,'ammu',22),(4,'aro',22),(5,'achu',22)")
-roll=int(input('enter your roll number : '))
+'''roll=int(input('enter your roll number : '))
 name=input('enter your name : ')
 age=int(input('enter your age : '))
 
-cur.execute("insert into std (roll_no,name,age)values(%s,%s,%s)",(roll,name,age))
+cur.execute("insert into std (roll_no,name,age)values(%s,%s,%s)",(roll,name,age))'''
+
+# cur.execute("select * from std order by name desc")
+'''cur.execute("select * from std where name like '_k%'")
+data=cur.fetchall()
+for i in data:
+    print(i)'''
+try:
+    cur.execute("create table mark (roll_no int,sub text,mark int)")
+except:
+    pass
+cur.execute("insert into mark(roll_no,sub,mark)values(1,'cs',70),(1,'chem',49),(2,'phy',58),(4,'eng',45)")
+cur.execute("select std.roll_no,std.name,std.age,mark.sub,mark.mark from std cross join mark")
+data=cur.fetchall()
+for i in data:
+    print(i)
+cur.execute("select name,count(age) from std group by name")
+data=cur.fetchall()
+for i in data:
+    print(i)
